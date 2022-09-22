502e6bda823663ba62449cb3f9de1bb61e48ed1d
Carletti <carletti@Carlettis-MacBook-Pro.local>
2022-09-22 01:10:47 -0300

    adding new casks

--- config.sh
+++ config.sh
@@ -3,11 +3,14 @@
 # Pre-install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+
 # Internal ToolsSet-up
 brew install git
 brew install zsh zsh-completions
 brew install mas
 brew link mas
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Mac Applications
 
@@ -17,11 +20,14 @@ brew install --cask slack
 brew install --cask github
 brew install --cask gitify
 brew install --cask visual-studio
-brew cask install iterm2
-brew cask install postman
+brew install --cask iterm2
+brew install --cask postman
 brew install --cask figma
 brew install --cask zoom
 brew install --cask sf-symbols
+brew install --cask 1password
+brew install --cask franz
+brew install --cask clipmenu
 
 
 # iOS Development
